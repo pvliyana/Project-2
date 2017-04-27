1e49cbb2900d4fb0b1b9b97ab7ba5a3fb5693aef
parami <parami@paramis-iMac.local>
2017-04-27 08:36:38 -0700

    first commit

---
+++ SIT206Ass2_215421142/SIT206Ass2_215421142/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SIT206Ass2_215421142
+//
+//  Created by parami on 27/4/17.
+//  Copyright © 2017 Deakin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
